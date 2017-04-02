46ef787144aca75e760d32e391e58f7ac408040c
LNoe-lzy <m15222505516@163.com>
2017-04-02 17:18:09 +0800

    使用stylus

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -69,107 +69,89 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
 ul {
   list-style: none;
 }
 
-.wrapper {
-  display: flex;
-  flex-direction: column;
-}
-
-.wrapper>.add {
-  width: 100%;
-}
-
-.wrapper>.add>input {
-  width: 100%;
-  box-sizing: border-box;
-  padding: 10px 0 10px 45px;
-  font-size: 24px;
-  border: solid 1px #ededed;
-  font-weight: 100;
-}
-
-.wrapper>section {
-  display: flex;
-  flex-direction: column;
-}
-
-.wrapper>section>.list-item {
-  width: 100%;
-  box-sizing: border-box;
-  padding: 10px 10px;
-  font-size: 24px;
-  border-bottom: solid 1px #ededed;
-  border-left: solid 1px #ededed;
-  border-right: solid 1px #ededed;
-  display: flex;
-  align-items: center;
-  position: relative;
-}
-.wrapper>section>.list-item > .item {
-  -webkit-appearance: none;
-  position: relative;
-  width: 24px;
-  height: 24px;
-  outline: none;
-}
-.wrapper>section>.list-item > .item::after {
-  content: '';
-  width: 24px;
-  height: 24px;
-  display: inline-block;
-  background: url('../assets/checkbox_no.png');
-  background-size: cover;
-}
-.wrapper>section>.list-item > .item:checked::after {
-  content: '';
-  width: 24px;
-  height: 24px;
-  display: inline-block;
-  background: url('../assets/checkbox_on.png');
-  background-size: cover;
-}
-.wrapper>section>.list-item > label {
-  margin-left: 10px;
-  font-weight: 100;
-}
-.wrapper>section>.list-item > .close {
-  display: block;
-  width: 24px;
-  height: 24px;
-  background: url('../assets/close.png');
-  background-size: cover;
-  cursor: pointer;
-  position: absolute;
-  right: 20px;
-}
-.completed { 
-  color: #ededed;
-  text-decoration: line-through;
-}
-.wrapper > footer {
-  padding: 12px 0;
-  box-sizing: border-box;
-  border-left: solid 1px #ededed;
-  border-right: solid 1px #ededed;
-  border-bottom: solid 1px #ededed;
-  display: flex;
-  justify-content: center;
-}
-.wrapper > footer > button {
-  padding: 5px;
-  background: none;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  border: 1px solid #ededed;
-  border-radius: 5px;
-  margin-right: 15px;
-}
-.wrapper > footer > .actived {
-  border: 1px solid #F4606C;
-}
+.wrapper
+  display: flex
+  flex-direction: column
+  .add
+    width: 100%
+    input
+      width: 100%
+      box-sizing: border-box
+      padding: 10px 0 10px 45px
+      font-size: 24px
+      border: solid 1px #ededed
+      font-weight: 100
+  section
+    display: flex
+    flex-direction: column
+    .list-item
+      width: 100%
+      box-sizing: border-box
+      padding: 10px 10px
+      font-size: 24px
+      border-bottom: solid 1px #ededed
+      border-left: solid 1px #ededed
+      border-right: solid 1px #ededed
+      display: flex
+      align-items: center
+      position: relative
+      .item
+        -webkit-appearance: none
+        position: relative
+        width: 24px
+        height: 24px
+        outline: none
+        &::after
+          content: ''
+          width: 24px
+          height: 24px
+          display: inline-block
+          background: url('../assets/checkbox_no.png')
+          background-size: cover
+        &:checked::after
+          content: ''
+          width: 24px
+          height: 24px
+          display: inline-block
+          background: url('../assets/checkbox_on.png')
+          background-size: cover
+      label
+        margin-left: 10px
+        font-weight: 100
+      .close
+        display: block
+        width: 24px
+        height: 24px
+        background: url('../assets/close.png')
+        background-size: cover
+        cursor: pointer
+        position: absolute
+        right: 20px
+      .completed
+        color: #ededed
+        text-decoration: line-through
+  footer
+    padding: 12px 0
+    box-sizing: border-box
+    border-left: solid 1px #ededed
+    border-right: solid 1px #ededed
+    border-bottom: solid 1px #ededed
+    display: flex
+    justify-content: center
+    button
+      padding: 5px
+      background: none
+      border: none
+      outline: none
+      cursor: pointer
+      border: 1px solid #ededed
+      border-radius: 5px
+      margin-right: 15px
+    .actived
+      border: 1px solid #F4606C
 </style>
